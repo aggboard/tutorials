424ce3019265d6069709d6c3f058f0b8e0dbd1e7
AGG Board <theaggboard@gmail.com>
2024-12-15 17:56:12 -0500

    Add files via upload

---
+++ Entities/ExampleTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities;
+
+public partial class ExampleTable
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+}
