4e614c1363230fcfc791a44368bea991cc82f834
David Cardoso <david@dacard.io>
2024-12-15 17:59:03 -0500

    Folder adjustments!

---
+++ Postgres-NET-Example/ConsoleApp/Worker.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ExampleDbContext _context;
+
+        public Worker(ExampleDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Grab all example records and iterate through them
+            List<ExampleTable> records = _context.ExampleTables.ToList();
+
+            foreach (var record in records)
+            {
+                Console.WriteLine($"Record ID {record.Id}: {record.Name}");
+            }
+
+            Console.ReadKey();
+
+            return;
+        }
+    }
+}
